e1d5c6b25ed7aee42e262b6d69b0418c40309078
abdullah107189 <abdullah107189@gmail.com>
2025-09-13 21:03:41 +0600

    otp verified successfully done

--- src/app/modules/otp/otp.services.ts
+++ src/app/modules/otp/otp.services.ts
@@ -1,6 +1,8 @@
 import crypto from "crypto";
 import { redisClient } from "../../config/redis.config";
 import { sendEmail } from "../../utils/sendEmail";
+import AppError from "../../errorHelpers/AppError";
+import { User } from "../user/user.model";
 const OTP_EXPIRATION = 2 * 60; // 2minute
 
 const generateOtp = (length = 6) => {
@@ -29,7 +31,20 @@ const sendOTP = async (email: string, name: string) => {
   });
 };
 
-const verifyOTP = async (email: string, otp: string) => {};
+const verifyOTP = async (email: string, otp: string) => {
+  const redisKey = `otp:${email}`;
+  const savedOtp = await redisClient.get(redisKey);
+  if (!savedOtp) {
+    throw new AppError(401, "Invalid Code");
+  }
+  if (savedOtp !== otp) {
+    throw new AppError(401, "Invalid Code");
+  }
+  await Promise.all([
+    User.updateOne({ email }, { isVerified: true }, { runValidators: true }),
+    redisClient.del([redisKey])
+  ]);
+};
 
 export const OTPServices = {
   sendOTP,
